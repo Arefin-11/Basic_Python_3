353a015fd9eca6984c28e6050b62ab5155cc59df
Arefin Islam <arefin.ruet.eee16@gmail.com>
2020-08-25 16:14:00 +0600

    Create codeforce_(A)_Way Too Long Words.py

---
+++ codeforce_(A)_Way Too Long Words.py
@@ -0,0 +1,9 @@
+n = int(input())
+words=[]
+for i in range(n):
+    words.append(input())
+for word in words:
+    if len(word)>10:
+        print("{}{}{}".format(word[0],(len(word)-2),word[-1]))
+    else:
+        print(word)
